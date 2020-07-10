a96f3c79c56f56b40ee4292397281d780b1edd69
MatuteDV <matiasvieira0@gmail.com>
2020-07-10 20:39:38 -0300

    Tercer etapa lista para verificar

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# MercadoLibre-DH
+# MercadoLiebre-DH
 Ejercitación clases sincrónicas - Construcción sitio web
 
 ### Primer etapa - M04C02-Ejercitación (Lista para verificar)
 
-### Segunda etapa - M04C03-Ejercitación-2 ()
+### Segunda etapa - M04C03-Ejercitación (2) (Lista para verificar)
+
+### Tercer etapa - M04C04-Ejercitación (3) (Lista para verificar)
--- app.js
+++ app.js
@@ -2,10 +2,30 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.post('/', (req, res) => {
+    // res.send(req.body);
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/register', (req, res) => {   // Sólo de prueba sin uso.
+    res.send(req.body);
+});
+
 
 
 app.listen(3000, ()=> { console.log('Escuchando en el puerto 3000') })
